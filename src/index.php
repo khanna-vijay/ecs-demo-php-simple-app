fcff7e15444509e165d5ffa09b3589fec1c68dd0
vijay-khanna <35032140+vijay-khanna@users.noreply.github.com>
2019-05-15 21:41:09 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -17,7 +17,7 @@
             <div class="hero-unit">
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations</h2>
-                <p>21 Feb. Namaste Friends.. .Your PHP application is now running on a container in Amazon ECS. Hello Vijay. Hello World 123 </p>
+                <p>Hi There.... .Your PHP application is now running on a container in Amazon ECS. Hello Vijay. Hello World 123 </p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
