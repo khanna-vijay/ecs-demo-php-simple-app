14329bb168714e7f8b5d094b271df73b4023a44d
vijay-khanna <35032140+vijay-khanna@users.noreply.github.com>
2019-06-13 08:35:02 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -17,7 +17,7 @@
             <div class="hero-unit">
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations</h2>
-                <p>Hi There.Hello World...  </p>
+                <p>Hello World...  </p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
